897340bbe9843c1aa7578b5194f5acf49fc0fc4c
Jake Roberts <jake.shaun.roberts1994@hotmail.com>
2022-06-20 20:50:55 +0800

    first commit to set new branch

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,12 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+		'X-RapidAPI-Host': 'rawg-video-games-database.p.rapidapi.com'
+	}
+};
 
+fetch('https://rawg-video-games-database.p.rapidapi.com/games', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
