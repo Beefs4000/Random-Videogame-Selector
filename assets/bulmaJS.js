aba6cac6c1b274572bfcae3c803e23d8caf1c270
Beefs4000 <103645633+Beefs4000@users.noreply.github.com>
2022-06-27 21:34:16 +0800

    removed bulmaJS

--- assets/bulmaJS.js
+++ assets/bulmaJS.js
@@ -1,25 +1,82 @@
-bulmaCarousel.attach('#slider', {
-  slidesToScroll: 1,
-  slidesToShow: 3,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 5000,
-});
-
-
 //base url
 const base_url = "https://api.rawg.io/api/";
 const APIkey = '5b7c4e0608b6468984fa007b7f2896e2'
 
 
+function createCarousel() {
+  
+/* <section class="section">
+      <div class="container is-clipped">
+        <div id="slider"></div> */
 
-// document.addEventListener('DOMContentLoaded', function(){
-// function getGames(games){
-// fetch(`${base_url}games?page_size=1&search=call of duty modern warfare&key=${APIkey}`)
-// .then(response => response.json()
+/* <div class="card">
+            <div class="card-image">
+              <figure class="image is-16by9 is-covered">
+                <img
+                  src="https://www.denofgeek.com/wp-content/uploads/2019/12/halo-infinite.jpg?resize=768%2C432"
+                  alt=""
+                />
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="title is-3">
+                Halo Infinite
+              </div>
+              <div class="item__description">
+                The legendary Halo series returns with the most expansive Master Chief campaign yet and a ground-breaking free to play multiplayer experience.
+              </div>
+            </div>
+          </div> */
 
-// )}
-// }, false);
+  // create carousel
+  const sectionCarousel = $("<section>");
+  // const sectionCarousel = createElement('section');
+  
+  sectionCarousel.attr('class', 'section');
+  
+  const divClass = $("<div>");
+  divClass.attr('class', 'container is-clipped');
+  
+  const divId = $("<div>");
+  divId.attr('id', 'slider');
+  
+  // create Cards
+  const cardDiv = $("<div>");
+  cardDiv.attr('class', 'card');
+  
+  const cardImg = $("<div>");
+  cardImg.attr('class', 'card-image');
+  
+  // need to add game background image from API
+  const figureCard = $("<img>").wrap("<figure></figure>");
+  figureCard.attr('class', 'image is-16by9 is-covered');
+  
+  
+  // need to add game title from API
+  const contentDiv = $("<div>");
+  contentDiv.attr('class', 'title is-3');
+  
+  // need to add game description from API
+  const descriptionDiv = $("<div>");
+  descriptionDiv.attr('class', 'item__description');
+  
+  
+  sectionCarousel.append(divClass, divId, cardDiv, cardImg, figureCard, contentDiv, descriptionDiv);
+  
+  return sectionCarousel;
+  
+};
+
+// Need a for loop to create 10 
+
+$(function (){
+
+  for (let index = 0; index < 10; index++) {
+    
+    const carousel = createCarousel();
+    
+  };
+});
 
 
 //getting the month
@@ -66,16 +123,14 @@ const gameDetailsURL = (game_id) =>
   `${base_url}games/${game_id}?key=${APIkey}`;
 
 
-async function APItest(){
-  const response = await fetch(`${base_url}${popular_games}`);
-  const data = await response.json();
-  console.log(data);
-  // data.results.forEach((result) => console.log(result.results));
-}
+// async function APItest(){
+//   const response = await fetch(`${base_url}${popular_games}`);
+//   const data = await response.json();
+//   console.log(data);
+//   // data.results.forEach((result) => console.log(result.results));
+// }
 
-APItest();
-
-
+// APItest();
 
 
 
---
+++ assets/intBulma.js
@@ -0,0 +1,8 @@
+bulmaCarousel.attach('#slider', {
+    slidesToScroll: 1,
+    slidesToShow: 3,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+  
